c371209bd005398c5340428decf1814d936c4fc0
Francois-Xavier Lemire <francois-xavier.lemire@mail.mcgill.ca>
2015-09-29 18:54:42 -0400

    refactored Client.java

--- A1/src/main/java/socs/network/net/Client.java
+++ A1/src/main/java/socs/network/net/Client.java
@@ -14,33 +14,23 @@ public class Client extends Thread {
     private Socket _clientSocket;
     private RouterDescription _remoteRouterDescription;
     private RouterDescription _rd;
-    private String remoteRouterIP;
+    private String _remoteRouterIP;
 
     public Client(RouterDescription remoteRouter, RouterDescription rd) {
         _remoteRouterDescription = remoteRouter;
         _rd = rd;
-    }
+        _remoteRouterIP = _remoteRouterDescription.getSimulatedIPAddress();
 
-    @Override
-    public void start() {
         try {
-            short remotePort = _remoteRouterDescription.getProcessPortNumber();
-            remoteRouterIP = _remoteRouterDescription.getSimulatedIPAddress();
-
-            System.out.println("Connecting to " + remoteRouterIP);
-            _clientSocket = new Socket(_remoteRouterDescription.getProcessIPAddress(), remotePort);
+            System.out.println("Connecting to " + _remoteRouterIP);
+            _clientSocket = new Socket(_remoteRouterDescription.getProcessIPAddress(), _remoteRouterDescription.getProcessPortNumber());
             System.out.println("Just connected to " + _clientSocket.getRemoteSocketAddress());
-            super.start();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     public void run() {
-        connect();
-    }
-
-    private void connect() {
         sendHello();
         Util.receiveMessage(_clientSocket);
         _remoteRouterDescription.setStatus(RouterStatus.TWO_WAY);
@@ -49,7 +39,7 @@ public class Client extends Thread {
 
     private void sendHello() {
         try {
-            System.out.println("Sending HELLO message to " + remoteRouterIP + "...");
+            System.out.println("Sending HELLO message to " + _remoteRouterIP + "...");
             SOSPFPacket message = Util.makeMessage(_rd, _remoteRouterDescription, (short) 0);
             ObjectOutputStream out = new ObjectOutputStream(_clientSocket.getOutputStream());
             out.writeObject(message);
